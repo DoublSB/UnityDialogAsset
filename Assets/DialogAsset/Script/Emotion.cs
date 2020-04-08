25abda3ed08d7396fc10165a6ab2ea57a25936d5
DoublSB <tjgus080@gmail.com>
2020-04-08 21:48:00 +0900

    02 : 클래스 설계 (1)
    https://doublsb.tistory.com/81
    
    - Emotion Class 추가
    - Dictionary 우회
    - 그러나? Serializeable한 Dictionary일 뿐이었고?

---
+++ Assets/DialogAsset/Script/Emotion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Doublsb.Dialog
+{
+    [SerializeField]
+    public class Emotion : MonoBehaviour
+    {
+        //================================================
+        //Public Variable
+        //================================================
+        public Dictionary<string, Sprite> Data;
+
+
+        //================================================
+        //Private Variable
+        //================================================
+        [SerializeField]
+        private string[] _emotion;
+
+        [SerializeField]
+        private Sprite[] _sprite;
+
+
+        //================================================
+        //Private Method
+        //================================================
+        private void Awake()
+        {
+            _init_emotionList();
+        }
+
+        private void _init_emotionList()
+        {
+            Data = new Dictionary<string, Sprite>();
+
+            if (_emotion.Length != _sprite.Length)
+                Debug.LogError("Emotion and Sprite have different lengths");
+
+            for (int i = 0; i < _emotion.Length; i++)
+                Data.Add(_emotion[i], _sprite[i]);
+        }
+    }
+}
