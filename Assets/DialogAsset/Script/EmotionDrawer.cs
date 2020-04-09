9b6f6d719703d671960837e2abd8f935d6a7f6b8
DoublSB <tjgus080@gmail.com>
2020-04-09 23:50:59 +0900

    03 : Property Drawer 알아보기
    - 열심히 문서를 찾았다
    - EmotionDrawer 추가

---
+++ Assets/DialogAsset/Script/EmotionDrawer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Doublsb.Dialog
+{
+    [CustomPropertyDrawer(typeof(Emotion))]
+    public class EmotionDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty _emotion = property.FindPropertyRelative("_emotion");
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            // {
+
+            Rect NewRect = new Rect(position.position, new Vector2(position.width, 16));
+
+            for (int i = 0; i < _emotion.arraySize; i++)
+            {
+                NewRect = new Rect(NewRect.position + new Vector2(0, 20), NewRect.size);
+                EditorGUI.PropertyField(NewRect, _emotion.GetArrayElementAtIndex(i));
+            }
+
+            // }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return property.isExpanded ? 16 * 10 : 16;
+        }
+    }
+}
+#endif
